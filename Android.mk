2705455de89b2d7376d4a7b39a061df6201af1db
Vayruz Rafli <frost@nusantararom.org>
2022-12-05 09:24:49 +0000

    Revert "gps: Use project pathmap"
    This reverts commit e094ba8092cb43555beff229dfc0845ed163cf38.
    
    Signed-off-by: Vayruz Rafli <frost@nusantararom.org>
    Change-Id: I928b7982b076e8152f85b409961f98a984ad4a8d

--- Android.mk
+++ Android.mk
@@ -1,4 +1,3 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-gps))
 ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
 
 # Set required flags
@@ -40,4 +39,3 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 GNSS_SANITIZE_DIAG := cfi bounds null unreachable integer address
 
 endif # ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
-endif # pathmap
